195c2f3bb924d81df8f8de25abf6d577f602b30f
leafsoar <kltwjt@gmail.com>
2013-09-11 11:00:12 +0800

    update fight

--- projects/KaPai/Resources/main.js
+++ projects/KaPai/Resources/main.js
@@ -40,7 +40,7 @@ require("Utile.js");
 
 try{
 	// var uri = "172.100.104.204:1234";
-	var uri = "172.100.104.139:1234";
+	var uri = "172.100.104.192:1234";
 	// var uri = "ws://localhost:8082/echo;
 	Socket.getInstance().setUri(uri);
 	director = cc.Director.getInstance();
